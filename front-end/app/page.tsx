c072d41edfe133ed0d4acfff7d757771ea4cd439
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-11-13 14:57:30 +0800

    after initial deploy , i added environment variable

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -2,6 +2,8 @@ export default async function Home() {
  const res= await fetch(`${process.env.DATABASE_URL}`)
  const data= await res.json()
   return (
+      <>
       <div>{data.success}</div>  
+      <p>Hello world</p></>
   );
 }
