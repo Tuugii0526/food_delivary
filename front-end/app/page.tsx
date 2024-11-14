22fb4c1ce8b824e31053668e6f0beecfa16b9e42
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-11-14 21:01:24 +0800

    food delivary branch checking

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,9 +1,24 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 export default async function Home() {
- const res= await fetch(`${process.env.DATABASE_URL}`)
- const data= await res.json()
+  let data;
+  let error;
+  try {
+    const res = await fetch(`${process.env.DATABASE_URL}`);
+    data = await res.json();
+  } catch (error) {
+    error = `${error}`;
+    console.log("error :", error);
+  }
   return (
-      <>
-      <div>{data.success}</div>  
-      <p>Hello world</p></>
+    <>
+      
+      <Alert>
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+        <div>{data?.success}</div>
+        {error && <p className="text-red-600">{error}</p>}
+        </AlertDescription>
+      </Alert>
+    </>
   );
 }
