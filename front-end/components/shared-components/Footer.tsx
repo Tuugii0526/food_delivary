8e966be7cd36d022cdbdcac0e0f9af9336f27f9d
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-11-18 07:55:48 +0800

    try try ... THere are a lot to learn

--- front-end/components/shared-components/Footer.tsx
+++ front-end/components/shared-components/Footer.tsx
@@ -1,8 +1,70 @@
+import { poppins } from "@/app/fonts/fonts";
+import PineconeIcon from "../icons/PineconeIcon";
+import { FacebookIcon } from "../icons/FacebookIcon";
+import { InstagramIcon } from "../icons/InstagramIcon";
+import { Copyright, XCircle, XIcon } from "lucide-react";
+import { XIconNotLucide } from "../icons/TwitterIcon";
+const contracts = [
+  {
+    id: 1,
+    title: "Нүүр",
+  },
+  {
+    id: 2,
+    title: "Холбоо барих",
+  },
+  {
+    id: 3,
+    title: "Хоолны цэс",
+  },
+  {
+    id: 4,
+    title: "Үйлчилгээний нөхцөл",
+  },
+  {
+    id: 5,
+    title: "Хүргэлтийн бүс",
+  },
+  {
+    id: 6,
+    title: "Нууцлалын бодлого",
+  },
+];
 export default function Footer() {
   return (
-    <div className="w-full bg-[#18BA51]">
-      <div className="container bg-yellow-200">
-        Hi Iam footer
+    <div className="w-full h-fit py-32 bg-[#18BA51] ">
+      <div className="container *:text-white flex flex-col   gap-10 *:w-full">
+        <div className="flex gap-2 justify-center">
+          <PineconeIcon fill="white" />
+          <p className={`${poppins.className} text-xl leading-[30px]`}>
+            Food delivery
+          </p>
+        </div>
+        <div className="flex justify-between">
+          {contracts.map((contract) => (
+            <p
+              key={contract.id}
+              className="font-semibold text-base leading-[19px]"
+            >
+              {contract.title}
+            </p>
+          ))}
+        </div>
+        <div className="flex justify-center gap-[18px]">
+          <FacebookIcon />
+          <InstagramIcon />
+          <XIconNotLucide />
+        </div>
+        <hr className="bg-white" />
+        <div className="flex flex-col items-center gap-2">
+          <p className="flex items-center gap-2 font-normal text-base leading-[19px] ">
+            <Copyright />
+            2024 Pinecone Foods LLC
+          </p>
+          <p className="font-normal text-base leading-[19px] ">
+            Зохиогчийн эрх хуулиар хамгаалагдсан
+          </p>
+        </div>
       </div>
     </div>
   );
--- front-end/components/shared-components/Header.tsx
+++ front-end/components/shared-components/Header.tsx
@@ -1,7 +1,54 @@
-export default function Header(){
-    return (
-        <div className="container bg-yellow-100">
-          This is nav
+import { Search, ShoppingBag, UserIcon } from "lucide-react";
+import PineconeIcon from "../icons/PineconeIcon";
+const navs = [
+  {
+    id: 1,
+    title: "нүүр",
+  },
+  {
+    id: 2,
+    title: "хоолны цэс",
+  },
+  {
+    id: 3,
+    title: "хүргэлтийн бүс",
+  },
+];
+export default function Header() {
+  return (
+    <div className="container  flex items-center justify-between px-6 py-2">
+      <div className="flex gap-6 items-center">
+        <PineconeIcon />
+        <div className="flex gap-2">
+          {navs.map((nav) => (
+            <div
+              key={nav.id}
+              className="py-2 px-4 font-bold text-sm leading-4 text-black uppercase"
+            >
+              {nav.title}
+            </div>
+          ))}
         </div>
-    )
+      </div>
+      <div className="flex gap-2 *:px-2 *:py-1 *:flex *:gap-2">
+        <label htmlFor="search" className="w-[260px] border-2 border-white rounded-[8px] has-[:focus]:border-gray-300">
+          <Search />
+          <input
+            id="search"
+            type="text"
+            placeholder="Хайх"
+            className="outline-none"
+          />
+        </label>
+        <div>
+          <ShoppingBag />
+          <p className="font-bold text-sm leading-5">Сагс</p>
+        </div>
+        <div>
+          <UserIcon />
+          <p className="font-bold text-sm leading-5">Нэвтрэх</p>
+        </div>
+      </div>
+    </div>
+  );
 }
