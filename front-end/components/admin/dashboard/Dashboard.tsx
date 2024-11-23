dc83d43e9b3a301095742742d22864f4ad20c111
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-11-23 09:34:45 +0800

    pagination front-end is added

--- front-end/components/admin/dashboard/Dashboard.tsx
+++ front-end/components/admin/dashboard/Dashboard.tsx
@@ -1,11 +1,13 @@
 import { DashboardHeader } from "./DashboardHeader";
 import { DashboardTable } from "./DashboardTable";
+import { AdminPagination } from "./Pagination";
 
 export const Dashboard = () => {
   return (
     <div className="w-full h-fit px-6">
       <DashboardHeader />
       <DashboardTable />
+      <AdminPagination />
     </div>
   );
 };
