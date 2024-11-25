fb983a963c24a1c6d6fbb566d51f1d5c2b09d5f8
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-11-25 17:47:38 +0800

    be aware be alert

--- back-end/index.js
+++ back-end/index.js
@@ -11,5 +11,5 @@ app.use(cors());
 app.use("/api", foodRouter);
 app.use("/api", categoryRouter);
 app.listen(PORT, () => {
-  console.log(`Service started working `);
+  console.log(`Service started working : be aware be alert`);
 });
