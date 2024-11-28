0855bfdabb37cdbecee37972e8fe38ce96e5d203
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-11-28 09:49:22 +0800

    something stirs inside me

--- front-end/components/home/foods/FoodCardDialog.tsx
+++ front-end/components/home/foods/FoodCardDialog.tsx
@@ -25,7 +25,7 @@ export function FoodCardDialog({
   finalPrice: number;
 }) {
   const countPropsFunctions = useCount();
-  const [count] = countPropsFunctions;
+  // const [count] = countPropsFunctions;
   return (
     <Dialog>
       <DialogTrigger className="user-nav" asChild>
