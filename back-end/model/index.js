7dc390fe086dde5896127ec3b32dd9fddd35ee61
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-11-28 10:04:32 +0800

    accept it as it is

--- back-end/model/index.js
+++ back-end/model/index.js
@@ -1,5 +1,5 @@
 import Food from "./food.js";
 import Category from "./category.js";
-import Order from "./order.js";
+import Order from "./Order.js";
 import User from "./User.js";
 export { Food, Category, Order, User };
