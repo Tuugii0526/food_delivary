6f35e1134a17895dea5b284d44d93638716bd3da
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-11-30 23:42:48 +0800

    as it is not working, I changed the module name

---
+++ front-end/lib/data-all/CheckCategoryIconss.tsx
@@ -0,0 +1,25 @@
+import { BookOpen, ChefHatIcon, Clock, MenuSquare } from "lucide-react";
+
+const checkCategoryIcons = [
+  {
+    id: 1,
+    iconName: "BookOpen",
+    icon: <BookOpen />,
+  },
+  {
+    id: 2,
+    iconName: "Clock",
+    icon: <Clock />,
+  },
+  {
+    id: 3,
+    iconName: "ChefHatIcon",
+    icon: <ChefHatIcon />,
+  },
+  {
+    id: 4,
+    iconName: "MenuSquare",
+    icon: <MenuSquare />,
+  },
+];
+export { checkCategoryIcons };
