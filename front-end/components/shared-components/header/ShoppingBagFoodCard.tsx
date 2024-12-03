8bcd8232ef01c687d4af9d5e7bd992578d5ac19e
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-12-03 13:00:36 +0800

    observe yourself

--- front-end/components/shared-components/header/ShoppingBagFoodCard.tsx
+++ front-end/components/shared-components/header/ShoppingBagFoodCard.tsx
@@ -1,4 +1,4 @@
-import { CountTypeForCart } from "@/lib/types";
+// import { CountTypeForCart } from "@/lib/types";
 
 export const ShoppingBagFoodCard = () => {
   return <div>Hello I am shopping bag card</div>;
