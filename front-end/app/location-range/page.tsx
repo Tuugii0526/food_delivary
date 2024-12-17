0760eee0a1f65552928a10e6f9580972875a4a3a
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-12-17 10:12:40 +0800

    location range has begun

--- front-end/app/location-range/page.tsx
+++ front-end/app/location-range/page.tsx
@@ -1,5 +1,3 @@
-export default function Page(){
-    return (
-        <div>Hell Iam location range</div>
-    )
+export default function Page() {
+  return <div className="w-full container h-[400px] bg-slate-400 my-10"></div>;
 }
